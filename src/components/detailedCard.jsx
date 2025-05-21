caf389affca7245eeae28ae581c25ef4a573eee5
sebastian-freire <SEBASTIAN.FREIRE@correo.ucu.edu.uy>
2025-05-21 03:18:12 -0300

    checkpoint

--- src/components/detailedCard.jsx
+++ src/components/detailedCard.jsx
@@ -1,8 +1,51 @@
 import React from "react";
 import "./detailedCard.css";
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function DetailedCard({ product, params }) {
+  const [cantidad, setCantidad] = useState(1);
+  const navigate = useNavigate();
+
+  const sumarCantidad = () => {
+    const nuevaCantidad = cantidad + 1;
+    setCantidad(nuevaCantidad);
+  };
+  const restarCantidad = () => {
+    const nuevaCantidad = cantidad - 1;
+    setCantidad(nuevaCantidad);
+  };
+
+  const agregarAJson = async () => {
+    const nuevoStock = product.stock + cantidad;
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/comidas/${product.id}`,
+        {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ stock: nuevoStock })
+        }
+      );
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(
+          `Error al actualizar el stock: ${response.status} ${
+            response.statusText
+          } - ${errorData.message || ""}`
+        );
+      }
+
+      console.log("Stock actualizado exitosamente.");
+      navigate("/home");
+    } catch (error) {
+      console.error("Hubo un problema con la operación fetch:", error);
+      throw error;
+    }
+  };
+
   return (
     <>
       <div className="DetailCard">
@@ -11,6 +54,22 @@ function DetailedCard({ product, params }) {
         <span>${product.price}</span>
         <p>Product Stock: {product.stock}</p>
         <p>Product Code: {params.productId}</p>
+      </div>
+      <div className="DetailCardButtons">
+        <button
+          className="quantity-btn"
+          onClick={restarCantidad}
+          disabled={cantidad === 1}
+        >
+          ➖
+        </button>
+        <span className="display-cantidad">{cantidad}</span>
+        <button className="quantity-btn" onClick={sumarCantidad}>
+          ➕
+        </button>
+        <button className="add-to-cart-btn" onClick={agregarAJson}>
+          Agregar
+        </button>
       </div>
     </>
   );
